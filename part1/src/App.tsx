89090c72921bc6d05c208d35124fc5916de3f628
Frederico <fozg@gft.com>
2025-02-12 10:07:59 +0100

    1.7 unicafe, paso 2

--- part1/src/App.tsx
+++ part1/src/App.tsx
@@ -13,23 +13,34 @@ const Button = (props: ButtonProps) => {
   )
 }
 
+let all = 0
+let average = 0
+let positive = 0
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   const onClick = (newValue:number, option:number) => {
+    let newGood = good
+    let newBad = bad
     switch (option) {
       case 1:
         setNeutral(neutral + newValue)
         break;
       case 2:
+        newBad = bad + newValue
         setBad(bad + newValue)
         break;
       default:
-        setGood(good + newValue)
+        newGood = good + newValue
+        setGood(newGood)
         break;
     }
+    all = all + newValue
+    average = ((newGood * 1) + (newBad * -1)) / all
+    positive = (newGood / all) * 100
   }
 
   return (
@@ -44,9 +55,12 @@ const App = () => {
       </div>
       <div>
         <h2>Statistics</h2>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
+        <p>all: {all}</p>
+        <p>average: {average}</p>
+        <p>positive: {positive} %</p>
       </div>
     </>
   )
