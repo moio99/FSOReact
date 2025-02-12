d706d4109bf1be705dc43f72bdaf5c7ca4a340cf
Frederico <fozg@gft.com>
2025-02-12 11:13:53 +0100

    1.12 anecdotes, paso 1

--- part1/src/App.tsx
+++ part1/src/App.tsx
@@ -66,9 +66,22 @@ let average = 0;
 let positive = 0;
 
 const App = () => {
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0)
 
   const onClick = (newValue: number, option: number) => {
     let newGood = good;
@@ -91,6 +104,11 @@ const App = () => {
     positive = (newGood / all) * 100;
   };
 
+  const onClickNextAnecdote = () => {
+    const index = Math.floor(Math.random() * anecdotes.length);
+    setSelected(index)
+  };
+
   return (
     <>
       <div>
@@ -103,6 +121,10 @@ const App = () => {
       </div>
       <div>
         <Statistics goodVal={good} badVal={bad} neutralVal={neutral} />
+      </div>
+      <div>
+        <p>{anecdotes[selected]}</p>
+        <Button onClick={() => onClickNextAnecdote()} text='Next anecdote' />
       </div>
     </>
   );
