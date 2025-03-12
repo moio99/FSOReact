f045b1a49194612f01391a2452a7deae5ebdb11b
Frederico <fozg@gft.com>
2025-03-12 15:04:02 +0100

    6.3 step 1

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,37 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import { createStore } from 'redux'
-import counterReducer from './reducers/reducer'
+import { Provider } from 'react-redux'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
 import './index.css'
 
-const store = createStore(counterReducer)
+const store = createStore(reducer)
 
-const App = () => {
-  const handleDispatch = (type) => {
-    store.dispatch({ type })
-  }
-
-  const { good, ok, bad } = store.getState()
-
-  return (
-    <div>
-      <button onClick={() => handleDispatch('GOOD')}>good</button>
-      <button onClick={() => handleDispatch('OK')}>ok</button>
-      <button onClick={() => handleDispatch('BAD')}>bad</button>
-      <button onClick={() => handleDispatch('ZERO')}>reset stats</button>
-      <div>good {good}</div>
-      <div>ok {ok}</div>
-      <div>bad {bad}</div>
-    </div>
-  )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
