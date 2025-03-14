13605ff3218bae6fba99f83bd9afaf9efcc2a932
Frederico <fozg@gft.com>
2025-03-14 11:19:04 +0100

    6.15 step 2

--- part1/src/components/AnecdoteForm.jsx
+++ part1/src/components/AnecdoteForm.jsx
@@ -1,14 +1,20 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AddNotification = () => {
   const dispatch = useDispatch()
 
-  const add = (event) => {
+  const getId = () => (100000 * Math.random()).toFixed(0)
+
+  const add = async (event) => {
     event.preventDefault()
     const content = event.target.notification.value
     event.target.notification.value = ''
+    await anecdoteService.createNew({
+      content: content, id: getId(), votes: 0
+    })
     dispatch(createAnecdote(content))
     dispatch(setNotification(`Created '${content}'`))
   }
