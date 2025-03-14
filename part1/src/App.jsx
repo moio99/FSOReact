2af383bf07b8616d983d13592b098e777bc68ae5
Frederico <fozg@gft.com>
2025-03-14 10:45:17 +0100

    6.14 step 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react'
 import Filter from './components/Filter'
 import AnecdotesList from './components/AnecdoteList'
 import AddNotification from './components/AnecdoteForm'
 import AnecdotesInfo from './components/AnecdoteInfo'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes} from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdote => dispatch(setAnecdotes(anecdote))
+    )
+  }, [dispatch])
 
   return (
     <div>
--- part1/src/reducers/anecdoteReducer.js
+++ part1/src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice, current } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -17,14 +8,12 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
 const anecSlice = createSlice({
   // O createSlice é umha funcionalidade do Redux Toolkit que simplifica a criaçom de redutores no Redux.
   // Em vez de escrever um grande caso de switch num redutor tradicional, 
   // o createSlice permite definir o estado inicial, os redutores e as açons, todo num só local.
   name: 'anecdotes',
-  initialState: initialState,
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {   // Nom tem return porque nom crea um novo estado só o modifica
       // Esta funçom modifica o estado directamente utilizando state.push(newAnecdote),
@@ -45,8 +34,14 @@ const anecSlice = createSlice({
         a.id !== id ? a : changedAnecdote 
       )     
     },
+    appendAnecdote(state, action) {   // Para engadir as notas umha por umha dende o anecdoteService.getAll()
+      state.push(action.payload)
+    },
+    setAnecdotes(state, action) {     // Para engadir toas as notas dende o anecdoteService.getAll()
+      return action.payload
+    }
   },
 })
 
-export const { createAnecdote, vote, showInfo } = anecSlice.actions
+export const { createAnecdote, vote, showInfo, appendAnecdote, setAnecdotes } = anecSlice.actions
 export default anecSlice.reducer
