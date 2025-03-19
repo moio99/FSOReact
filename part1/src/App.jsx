d48abd2f6c314270e70eb5c63e66afd0bed684cf
Frederico <fozg@gft.com>
2025-03-19 12:49:26 +0100

    7.4 step 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,7 +5,10 @@ import {
   Routes, Route, Link, useMatch, useNavigate
 } from 'react-router-dom'
 import Menu from './componets/Menu'
+import About from './componets/About'
+import Footer from './componets/Footer'
 import AnecdotesInfo from './componets/AnecdotesInfo'
+import  { useField } from './hooks'
 import './index.css'
 
 const Anecdote = ({ anecdote }) => {
@@ -39,38 +42,16 @@ const AnecdoteList = ({ anecdotes }) => (
   </div>
 )
 
-const About = () => (
-  <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
-
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is &quot;a story with a point.&quot;</em>
-
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-  </div>
-)
-
-const Footer = () => (
-  <div>
-    Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
-
-    See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for the source code.
-  </div>
-)
-
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-  
+  const content = useField('content')
+  const author = useField('author')
+  const url = useField('url')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    props.addNew({ content, author, info, votes: 0 })
+    console.log(name)
+    props.addNew({ content: content.value, author: author.value, info: url.value, votes: 0 })
     navigate('/')
   }
 
@@ -80,15 +61,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input type={content.type} value={content.value} onChange={content.onChange} /> 
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input type={author.type} value={author.value} onChange={author.onChange} /> 
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input type={url.type} value={url.value} onChange={url.onChange} /> 
         </div>
         <button>create</button>
       </form>
